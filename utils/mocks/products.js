a356431a94b542fcc117a74bc110c803a3dd4edf
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-01-12 18:48:54 -0300

    se arregla problema con metodo create

--- utils/mocks/products.js
+++ utils/mocks/products.js
@@ -1,10 +1,10 @@
 const productMocks = [
-    { id: '1', image: 'assets/images/camiseta.png', title: 'Camiseta', price: 500, description: 'very good' },
-    { id: '2', image: 'assets/images/hoodie.png', title: 'Hoodie', price: 600, description: 'very good' },
-    { id: '3', image: 'assets/images/mug.png', title: 'Mug', price: 300, description: 'very good' },
-    { id: '4', image: 'assets/images/pin.png', title: 'Pin', price: 400, description: 'very good' },
-    { id: '5', image: 'assets/images/stickers1.png', title: 'Stickers1', price: 100, description: 'very good' },
-    { id: '6', image: 'assets/images/stickers2.png', title: 'Stickers2',  price: 200, description: 'very good' }
+    { "id": "1", "image": "assets/images/camiseta.png", "title": "Camiseta", "price": "500", "description": "very good" },
+    { "id": "2", "image": "assets/images/hoodie.png", "title": "Hoodie", "price": "600", "description": "very good" },
+    { "id": "3", "image": "assets/images/mug.png", "title": "Mug", "price": "300", "description": "very good" },
+    { "id": "4", "image": "assets/images/pin.png", "title": "Pin", "price": "400", "description": "very good" },
+    { "id": "5", "image": "assets/images/stickers1.png", "title": "Stickers1", "price": "100", "description": "very good" },
+    { "id": "6", "image": "assets/images/stickers2.png", "title": "Stickers2",  "price": "200", "description": "very good" }
 ];
 
 module.exports = productMocks;
